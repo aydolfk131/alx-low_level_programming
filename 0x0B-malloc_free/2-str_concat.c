32b169f1c1097182e1a5343848467ec5bce6a0e3
adolf <kajeseadolf@gmail.com>
2023-10-31 17:03:54 -0700

    100-argstostr.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,9 +4,9 @@
  * str_concat - A function that get ends of input and add together
  * @s1: input
  * @s2: input2
- * Return: concat of s1 and s2	
+ * Return: concat of s1 and s2
  */
-char *str_concat(char *s1, char *s2)		
+char *str_concat(char *s1, char *s2)
 {
 char *conct;
 int i, j;
@@ -15,25 +15,25 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-i = ci = 0;
+i = j = 0;
 while (s1[i] != '\0')
 i++;
 while (s2[j] != '\0')
-ci++;
+j++;
 conct = malloc(sizeof(char) * (i + j + 1));
 if (conct == NULL)
 return (NULL);
 i = j = 0;
-while (s1[i] != '\0')	
+while (s1[i] != '\0')
 {
 conct[i] = s1[i];
-i++;		
+i++;
 }
-while (s2[j] != '\0')	
+while (s2[j] != '\0')
 {
 conct[i] = s2[j];
 i++, j++;
 }
 conct[i] = '\0';
-return (conct);		
+return (conct);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"	
+#include "main.h"
 /**
  * free_grid - A function that frees 2d array
  * @grid: grid
  * @height: height of grid
  * Description: frees memory
- * Return: nothing	
- *	
- */	
+ * Return: nothing
+ *
+ */
 void free_grid(int **grid, int height)
 {
 int i;
 
-for (i = 0; i < height; i++)		
+for (i = 0; i < height; i++)
 {
 free(grid[i]);
 }
-  free(grid);		
+free(grid);
 }
-
