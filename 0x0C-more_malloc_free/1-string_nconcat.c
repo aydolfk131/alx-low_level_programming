05cd21782cff63c60a3bbf7ee36f67625795a335
adolf <kajeseadolf@gmail.com>
2023-11-03 02:14:34 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - A fun that concatenatestwo strings
+ * @s1: input string
+ * @s2: string
+ * @n: number of bytes allocated
+ * Return: a pointer to the string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	int len1 = 0;
+	int len2 = 0;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i]; != '\0'; i++)
+		len2++;
+
+	ptr = malloc(sizeof(char) * (len1 + n) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	if (n >= len)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			ptr[i] = s1[i];
+		for (i = 0; s2[i]; != '\0'; i++)
+			ptr[len1 + i] = s2[i];
+		ptr[len1 + i] = '\0';
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			ptr[i] = s1[i];
+		for (i = 0; i < n; i++)
+			ptr[len1 + i] = s2[i];
+		ptr[len1 + 1] = '\0';
+	}
+	return (ptr);
+}
