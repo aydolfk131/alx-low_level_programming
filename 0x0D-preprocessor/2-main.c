b163e25c82901c9e67c03abe0983af4c4943541c
adolf <kajeseadolf@gmail.com>
2023-11-06 10:56:36 -0800

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+int _putchar(char c);
 /**
  * main - Prints the name of file it was compiled from
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s,\n", __FILE__);
+	const char *filename = __FILE__;
+	int i;
+
+	for (i = 0; filename[i] != '\0'; i++)
+		_putchar(filename[i]);
+
+	_putchar('\n');
+
 	return (0);
+}
+
+int _putchar(char c)
+{
+	return putchar(c);
 }
