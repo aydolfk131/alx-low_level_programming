9aed74747008c4d74080ef8a5c412b6b030c08e0
adolf <kajeseadolf@gmail.com>
2023-11-27 21:51:45 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,8 +19,7 @@ char *creates_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
